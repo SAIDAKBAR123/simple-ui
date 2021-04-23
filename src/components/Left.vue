d5a3746d6173c1b1b9ff98c1d5fcfd7bf245dc98
Saidakbar Makhmudkhujaev <familyhomeuz@gmail.com>
2021-04-23 17:05:00 +0500

    mode changed

---
+++ src/components/Left.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+       <v-card tile flat color="white" height="100%" width="100%">
+           <v-card-text>
+             <v-img :src="require('../assets/images/product1.png')"></v-img>
+           </v-card-text>
+        </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
